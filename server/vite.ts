659853fbd42aff1473adee48afe5ef9221d8baa8
坂本健太 <sakamotokenta@MacBook-Pro-2.local>
2025-06-26 00:42:19 +0900

    fix: update vite config

--- server/vite.ts
+++ server/vite.ts
@@ -3,8 +3,13 @@ import fs from "fs";
 import path from "path";
 import { createServer as createViteServer, createLogger } from "vite";
 import { type Server } from "http";
+import { fileURLToPath } from "url"; //
 import viteConfig from "../vite.config";
 import { nanoid } from "nanoid";
+
+// ↓ __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const viteLogger = createLogger();
 
@@ -46,7 +51,7 @@ export async function setupVite(app: Express, server: Server) {
 
     try {
       const clientTemplate = path.resolve(
-        import.meta.dirname,
+        __dirname,
         "..",
         "client",
         "index.html",
@@ -68,7 +73,7 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(import.meta.dirname, "../public"); //
+  const distPath = path.resolve(__dirname, "../public"); //
 
   if (!fs.existsSync(distPath)) {
     throw new Error(`Could not find the build directory: ${distPath}`);
